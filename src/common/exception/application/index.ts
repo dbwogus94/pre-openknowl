58b51a70a65881c35b56e2ddcfae5df5bf3c6003
jae hyun <1994dbwogus@gmail.com>
2025-08-11 00:56:44 +0900

    feat: 에러 처리를 계층적으로 진행하기 위한 설정 완료
    - application 예외 정의
    - http 예외 정의
    - express 글로벌 예외 미들웨어에서 예외가 응답되도록 설정하는 코드 추가

---
+++ src/common/exception/application/index.ts
@@ -0,0 +1,5 @@
+export * from './application.exception';
+export * from './conflict-status.exception';
+export * from './invalid-parameter.exception';
+export * from './resource-not-found.exception';
+export * from './run-time.exception';
--- src/common/models/index.ts
+++ src/common/models/index.ts
@@ -1,1 +1,2 @@
-export * from './serviceResponse';
+export * from './response.dto';
+export * from './ServiceResponse';
