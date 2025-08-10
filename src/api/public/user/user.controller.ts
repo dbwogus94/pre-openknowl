0be4f22aa189fce3f38d3f5228098cede5d0fb8a
jae hyun <1994dbwogus@gmail.com>
2025-08-11 04:40:39 +0900

    feat: UserController#getUser isAuth에서 넘어오는 값 변경으로 인한 대응 작업 완료

--- src/api/public/user/user.controller.ts
+++ src/api/public/user/user.controller.ts
@@ -8,7 +8,7 @@ export class UserController {
 
 	public getUser: RequestHandler = async (req: Request, res: Response) => {
 		// '/users/me' 경로는 JWT에서 주입된 req.id를 사용
-		const id = req.id as string;
+		const id = req.user?.id as string;
 		const userInfo = await this.userService.getById(id);
 		return res.status(StatusCodes.OK).send(GetUserResponseDto.of(userInfo));
 	};
